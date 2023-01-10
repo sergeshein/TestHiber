6870d7d3e79b948d11a82ca0d120bac4f9c79409
eralash911 <unreal725@gmail.com>
2023-01-10 05:56:16 +0300

    added controllers, views

--- src/main/java/com/example/testhiber/service/UserService.java
+++ src/main/java/com/example/testhiber/service/UserService.java
@@ -3,7 +3,9 @@ package com.example.testhiber.service;
 
 import com.example.testhiber.dto.UserDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService extends UserDetailsService { //security
     boolean save(UserDto userDto);
 }
