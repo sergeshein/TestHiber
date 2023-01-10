08a5843bbf806cf3911222a5256a83d487a9372d
eralash911 <unreal725@gmail.com>
2023-01-11 00:33:59 +0300

    added  views

--- src/main/java/com/example/testhiber/config/SecurityConfig.java
+++ src/main/java/com/example/testhiber/config/SecurityConfig.java
@@ -6,38 +6,52 @@ import jakarta.persistence.Basic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
+public class SecurityConfig  {
+
     private UserService userService;
 
-    public void setUserService(UserService userService) {
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void setUserService(UserService userService, UserDetailsService userDetailsService) {
         this.userService = userService;
+        this.userDetailsService = userDetailsService;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
-    @Basic
-    private AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(userService);
-        auth.setPasswordEncoder(passwordEncoder());
-        return auth;
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(authenticationProvider());
+//    }
+//    @Basic
+//    private AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+//        auth.setUserDetailsService(userService);
+//        auth.setPasswordEncoder(passwordEncoder());
+//        return auth;
+//    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
@@ -45,8 +59,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests()
+//                    .antMatchers("/users/new").hasAuthority(Role.ADMIN.name())
+//                    .anyRequest().permitAll()
+//                .and()
+//                    .formLogin()
+//                    .loginPage("/login")
+//                    .loginProcessingUrl("/auth")
+//                    .permitAll()
+//                .and()
+//                    .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                    .logoutSuccessUrl("/").deleteCookies("JSESSIONID")
+//                    .invalidateHttpSession(true)
+//                .and()
+//                    .csrf().disable();
+//
+//        super.configure(http);
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                     .antMatchers("/users/new").hasAuthority(Role.ADMIN.name())
                     .anyRequest().permitAll()
@@ -62,6 +96,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .csrf().disable();
 
-        super.configure(http);
+        return http.build();
     }
+
+//        http.csrf().disable().cors().disable().authorizeHttpRequests()
+//                .requestMatchers("/user/new").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2ResourceServer();
+//        return http.build();
+
 }
