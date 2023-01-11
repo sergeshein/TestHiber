f7fa8de1b4345b0c4be2a1cf94a644d2a83597b9
eralash911 <unreal725@gmail.com>
2023-01-11 22:57:53 +0300

    added  views

--- src/main/java/com/example/testhiber/config/SecurityConfig.java
+++ src/main/java/com/example/testhiber/config/SecurityConfig.java
@@ -2,19 +2,14 @@ package com.example.testhiber.config;
 
 import com.example.testhiber.entity.Role;
 import com.example.testhiber.service.UserService;
-import jakarta.persistence.Basic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -37,17 +32,6 @@ public class SecurityConfig  {
         this.userDetailsService = userDetailsService;
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(authenticationProvider());
-//    }
-//    @Basic
-//    private AuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-//        auth.setUserDetailsService(userService);
-//        auth.setPasswordEncoder(passwordEncoder());
-//        return auth;
-//    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -59,25 +43,7 @@ public class SecurityConfig  {
         return new BCryptPasswordEncoder();
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests()
-//                    .antMatchers("/users/new").hasAuthority(Role.ADMIN.name())
-//                    .anyRequest().permitAll()
-//                .and()
-//                    .formLogin()
-//                    .loginPage("/login")
-//                    .loginProcessingUrl("/auth")
-//                    .permitAll()
-//                .and()
-//                    .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                    .logoutSuccessUrl("/").deleteCookies("JSESSIONID")
-//                    .invalidateHttpSession(true)
-//                .and()
-//                    .csrf().disable();
-//
-//        super.configure(http);
-//    }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -95,16 +61,8 @@ public class SecurityConfig  {
                     .invalidateHttpSession(true)
                 .and()
                     .csrf().disable();
-
-
         return http.build();
     }
 
-//        http.csrf().disable().cors().disable().authorizeHttpRequests()
-//                .requestMatchers("/user/new").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .oauth2ResourceServer();
-//        return http.build();
 
 }
