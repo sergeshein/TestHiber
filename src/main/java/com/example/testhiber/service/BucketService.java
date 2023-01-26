1b9c95dfa7ee142bf63e47b0afec1ba09c8080b9
eralash911 <unreal725@gmail.com>
2023-01-26 20:48:57 +0300

    added some fixes little bit

--- src/main/java/com/example/testhiber/service/BucketService.java
+++ src/main/java/com/example/testhiber/service/BucketService.java
@@ -3,9 +3,10 @@ package com.example.testhiber.service;
 import com.example.testhiber.dto.BucketDto;
 import com.example.testhiber.entity.Bucket;
 import com.example.testhiber.entity.User;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface BucketService {
     Bucket createBucket(User user, List<Long>productIds);
     void addProduct(Bucket bucket, List<Long>productIds);
